ab52434ff3876d4598547c10db72162cc2a0773e
Jenny Aguilar <ingecomaguilar@gmail.com>
2020-12-19 11:11:12 -0300

    Insert new artist form implemented

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -475,14 +475,37 @@ def create_artist_submission():
   # called upon submitting the new artist listing form
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
+    error= False
+    try:
+      name = request.form.get('name')
+      city = request.form.get('city')
+      state = request.form.get('state')
+      phone = request.form.get('phone')
+      site_link = request.form.get('site_link')
+      facebook_link = request.form.get('facebook_link')
+      seeking_venue = True
+      seeking_description = 'Estamos buscando'     
+      image_link = request.form.get('image_link')
+      artist= Artist(name=name, city=city, state=state, phone=phone, site_link=site_link, facebook_link=facebook_link, seeking_venue=seeking_venue, seeking_description=seeking_description, image_link=image_link)
+      db.session.add(artist)
+      db.session.commit()
+    except:
+      error=True
+      db.session.rollback()
+    finally:
+      db.session.close()
+    if error:
+      flash('An error occurred. Artist could not be insert.') 
+    else:
+       flash('Artist ' + request.form['name'] + ' was successfully listed!')
+    
+    return render_template('pages/home.html')
 
   # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
+  #flash('Artist ' + request.form['name'] + ' was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
-  return render_template('pages/home.html')
-
-
+ 
 #  Shows
 #  ----------------------------------------------------------------
 
