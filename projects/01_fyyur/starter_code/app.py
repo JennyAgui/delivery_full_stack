1d3eaabd5cc37e0587e415ed7f07c8bc0a29beb8
Jenny Aguilar <ingecomaguilar@gmail.com>
2020-12-20 00:41:09 -0300

    Show shows listed OK. Formated datetime-string

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -35,7 +35,7 @@ class Show(db.Model):
     __tablename__ = 'show'    
     artist_id= db.Column(db.Integer, db.ForeignKey('artist.id_artist'), primary_key=True) 
     venue_id = db.Column( db.Integer, db.ForeignKey('venue.id_venue'), primary_key=True)
-    date = db.Column(db.DateTime(), unique=True, nullable=True)
+    start_time = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
     venue_child = db.relationship("Venue", back_populates="artists") 
     artist_parent = db.relationship("Artist", back_populates="venues") 
 
@@ -328,43 +328,22 @@ def shows():
   # displays list of shows at /shows
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "venue_id": 1,
-    "venue_name": "The Musical Hop",
-    "artist_id": 4,
-    "artist_name": "Guns N Petals",
-    "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    "start_time": "2019-05-21T21:30:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 5,
-    "artist_name": "Matt Quevedo",
-    "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    "start_time": "2019-06-15T23:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-01T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-08T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-15T20:00:00.000Z"
-  }]
-  return render_template('pages/shows.html', shows=data)
+    shows = Show.query.all()
+    try:    
+      data = []  
+      for show in shows:
+          data.append({
+                "venue_id": show.venue_id,
+                "venue_name": Venue.query.filter_by(id_venue=show.venue_id).first().name,
+                "artist_id": show.artist_id,
+                "artist_name": Artist.query.filter_by(id_artist=show.artist_id).first().name,
+                "artist_image_link": Artist.query.filter_by(id_artist=show.artist_id).first().image_link,
+                "start_time": str(show.start_time)
+          })
+    except Exception as e:
+        print(e)
+        pass
+    return render_template('pages/shows.html', shows=data)
 
 @app.route('/shows/create')
 def create_shows():
@@ -380,8 +359,8 @@ def create_show_submission():
     try:
       artist_id = request.form.get('artist_id')
       venue_id = request.form.get('venue_id')
-      date = request.form.get('start_time')    
-      show= Show(artist_id=artist_id, venue_id=venue_id, date=date)
+      start_time = request.form.get('start_time')    
+      show= Show(artist_id=artist_id, venue_id=venue_id, start_time=start_time)
       db.session.add(show)
       db.session.commit()
     except:
